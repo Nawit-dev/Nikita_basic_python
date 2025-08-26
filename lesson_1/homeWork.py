8bb5e76cb35f18b9525718308b5158bb157fa5e9
nikita <feo.nickita@yandex.ru>
2025-08-26 16:26:09 +0300

    homework 1

---
+++ lesson_1/homeWork.py
@@ -0,0 +1,30 @@
+# a = 7
+# b = 8.34
+# str_1 = "легко в ИТ"
+# b = a
+# a = 8
+# print(b)
+
+# a = b = c = 0
+# a, b, c = 0,0,0
+
+# a,b = 8,10
+# a,b = b,a
+# print(a)
+# print(b)
+
+# print(type(a))
+# print(type(str_1))
+
+# message = "Привет, Даниил"
+# count = 0
+# Count = 0
+
+
+# a = c = 4
+# b, d  = "Какая-нибудь строка","Вторая строка"
+#
+# print(id(a))
+# print(id(b))
+# print(id(c))
+# print(id(d))
